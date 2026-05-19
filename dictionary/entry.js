36a465888e672f23260b15c1973aff6c71166a80
yamauchi09 <y-yota-02vpr6@g.bears.ed.jp>
2026-05-19 16:21:41 +0900

    Delete dictionary/entry.js

--- dictionary/entry.js
+++
@@ -1,9 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const file = params.get('file');
-
-fetch(file)
-  .then(response => response.text())
-  .then(markdown => {
-    document.getElementById('content').innerHTML =
-      marked.parse(markdown);
-  });
