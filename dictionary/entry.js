c60e4774f95015a4718e389abfc63a89ec0a3853
yamauchi09 <y-yota-02vpr6@g.bears.ed.jp>
2026-05-19 15:58:53 +0900

    Update entry.js

--- dictionary/entry.js
+++ dictionary/entry.js
@@ -1,1 +1,9 @@
+const params = new URLSearchParams(window.location.search);
+const file = params.get('file');
 
+fetch(file)
+  .then(response => response.text())
+  .then(markdown => {
+    document.getElementById('content').innerHTML =
+      marked.parse(markdown);
+  });
